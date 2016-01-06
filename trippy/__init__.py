20150e049b0f8e15f3a872a427eafcdb33b46689
fraserw <westhefras@gmail.com>
2016-01-06 12:44:17 +0000

    --moved versioning here --fixes to correct import of all the modules

--- trippy/__init__.py
+++ trippy/__init__.py
@@ -1,2 +1,5 @@
-__all__=['psf','pill','bgFinder','utils','psfStarChooser']
+
+__version__'0.1.1'
+__author__='Wesley Fraser (github: fraserw)'
+__all__=['psf','pill','bgFinder','utils','psfStarChooser','MCMCfit','scamp']
 import psf,pill,bgFinder
