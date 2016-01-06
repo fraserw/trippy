3df4599a949fb5b7a95f261aaa93649f629b2cfd
fraserw <westhefras@gmail.com>
2016-01-06 12:47:36 +0000

    --derp

--- trippy/__init__.py
+++ trippy/__init__.py
@@ -1,5 +1,5 @@
 
-__version__'0.1.1'
+__version__='0.1.1'
 __author__='Wesley Fraser (github: fraserw)'
 __all__=['psf','pill','bgFinder','utils','psfStarChooser','MCMCfit','scamp']
 import psf,pill,bgFinder
