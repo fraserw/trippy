56fe54221c038752973f5ee27f845bcab217399e
fraserw <westhefras@gmail.com>
2016-01-14 18:38:08 +0000

    --version bump

--- trippy/__init__.py
+++ trippy/__init__.py
@@ -1,5 +1,5 @@
 
-__version__='0.2.4'
+__version__='0.2.5'
 __author__='Wesley Fraser (github: fraserw)'
 __all__=['psf','pill','bgFinder','utils','psfStarChooser','MCMCfit','scamp']
 import psf,pill,bgFinder
