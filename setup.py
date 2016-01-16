e3da446efd77f3a51351963ab6fd11adf853995e
fraserw <westhefras@gmail.com>
2016-01-16 11:47:23 +0000

    --bump to v0.2.6! Basically at a point of useable by the masses. Time for the champagne.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dependencies = ['numpy >= 1.6.1',
 setup(
   name = 'trippy',
   packages = ['trippy','trippy.scamp','trippy.MCMCfit'],
-  version = '0.2.5',
+  version = '0.2.6',
   description = 'Pill aperture photometry for trailed astronomical sources',
   author = 'Wesley Fraser',
   author_email = 'westhefras@gmail.com',
