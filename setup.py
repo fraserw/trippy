cf7d80996fcb48d4648b9777d0a03068c8c9d330
fraserw <westhefras@gmail.com>
2016-03-31 09:37:00 +0100

    --updated to versino 0.4, the published version!

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dependencies = ['numpy >= 1.6.1',
 setup(
   name = 'trippy',
   packages = ['trippy','trippy.scamp','trippy.MCMCfit'],
-  version = '0.3',
+  version = '0.4',
   description = 'Pill aperture photometry for trailed astronomical sources',
   author = 'Wesley Fraser',
   author_email = 'westhefras@gmail.com',
