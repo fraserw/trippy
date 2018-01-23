c989dbba3d67f2a828566895597018814c0b604d
fraserw <westhefras@gmail.com>
2018-01-23 09:48:22 +0000

    --minor bug fix, and swap to np. rather than num.

--- trippy/MCMCfit/MCMCfit.py
+++ trippy/MCMCfit/MCMCfit.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Wesley Fraser (@wtfastro, github: fraserw <westhefras@gmail.com>), Academic email: wes.fraser@qub.ac.uk'
 
-import numpy as num, scipy as sci,emcee
+import numpy as np, scipy as sci,emcee
 from trippy import bgFinder
 import pickle
 
@@ -31,16 +31,16 @@ def lnprob(r,dat,lims,psf,ue,useLinePSF, verbose=False, other=None):
     psf.nForFitting+=1
     if other <> None:
         (x,y,amp) = other[:]
-    if len(r)==3:
+    if len(r) == 3:
         (x, y, amp) = r
-    elif len(r)==2:
+    elif len(r) == 2:
         (x,y) = r
-    elif len(r)==1:
+    elif len(r) == 1:
         amp = r[0]
     (a,b)=dat.shape
-    if  amp<=0 or x>=b or x<=0 or y<=0 or y>=a: return -num.inf
-    diff=psf.remove(x,y,amp,dat,useLinePSF=useLinePSF)[lims[0]:lims[1],lims[2]:lims[3]]
-    chi=-0.5*num.sum(diff**2/ue[lims[0]:lims[1],lims[2]:lims[3]]**2)
+    if  amp <= 0 or x >= b or x <= 0 or y <= 0 or y >= a: return -np.inf
+    diff = psf.remove(x,y,amp,dat,useLinePSF=useLinePSF)[lims[0]:lims[1],lims[2]:lims[3]]
+    chi = -0.5*np.sum(diff**2/ue[lims[0]:lims[1],lims[2]:lims[3]]**2)
     if verbose: print '{:6d} {:8.3f} {:8.3f} {:8.3f} {:10.3f}'.format(psf.nForFitting,x,y,amp,chi)
     return chi
 
@@ -51,27 +51,52 @@ def lnprob_varRateAngle(r,dat,lims,psf,ue,useLinePSF, exptime, pixScale, verbose
     """
     psf.nForFitting+=1
     (x, y, amp, rate, angle) = r[:]
-    (a,b)=dat.shape
-    if  amp<=0 or x>=b or x<=0 or y<=0 or y>=a or angle>90 or angle<-90 or rate <0: return -num.inf
+    (a,b) = dat.shape
+    if  amp<=0 or x>=b or x<=0 or y<=0 or y>=a or angle>90 or angle<-90 or rate <0: return -np.inf
 
     psf.line(rate, angle, exptime/3600., pixScale = pixScale, useLookupTable = True, verbose = False)
 
-    diff=psf.remove(x,y,amp,dat,useLinePSF=useLinePSF)[lims[0]:lims[1],lims[2]:lims[3]]
-    chi=-0.5*num.sum(diff**2/ue[lims[0]:lims[1],lims[2]:lims[3]]**2)
+    diff = psf.remove(x,y,amp,dat, useLinePSF = useLinePSF)[lims[0]:lims[1],lims[2]:lims[3]]
+    chi = -0.5*np.sum(diff**2/ue[lims[0]:lims[1],lims[2]:lims[3]]**2)
+    if verbose: print '{:6d} {:8.3f} {:8.3f} {:8.3f} {:8.3f} {:8.3f} {:10.3f}'.format(psf.nForFitting,x,y,amp,rate,angle,chi)
+    return chi
+
+
+def lnprob_varRateAngle_LSSTHACK(r,dat,lims,psf,ue,useLinePSF, exptime, pixScale, verbose=False):
+    """
+    for now because I am lazy, rates, exptime, and pixScale should be given in "/hr, seconds, and "/pix
+    """
+    dT = 5.0
+
+    psf.nForFitting += 1
+    (x, y, amp, rate, angle) = r[:]
+    (a,b) = dat.shape
+    if  amp<=0 or x>=b or x<=0 or y<=0 or y>=a or angle>90 or angle<-90 or rate <0: return -np.inf
+
+    psf.line(rate, angle, exptime/3600., pixScale = pixScale, useLookupTable = True, verbose = False)
+
+    dl = rate * (exptime / 2.0 + dT/2.0) / 3600.0 / pixScale
+    dx = dl * np.cos(angle * np.pi / 180.0)
+    dy = dl * np.sin(angle * np.pi / 180.0)
+
+    d = psf.remove(x-dx/2.0,y-dy/2.0,amp,dat,useLinePSF=useLinePSF)
+    diff = psf.remove(x+dx/2.0,y+dy/2.0,amp,d,useLinePSF=useLinePSF)[lims[0]:lims[1],lims[2]:lims[3]]
+
+    chi = -0.5*np.sum(diff**2/ue[lims[0]:lims[1],lims[2]:lims[3]]**2)
     if verbose: print '{:6d} {:8.3f} {:8.3f} {:8.3f} {:8.3f} {:8.3f} {:10.3f}'.format(psf.nForFitting,x,y,amp,rate,angle,chi)
     return chi
 
 
 def lnprobDouble(r,dat,lims,psf,ue,useLinePSF,verbose=False):
     psf.nForFitting+=1
-    (A,B)=dat.shape
-    (X,Y,AMP,x,y,amp)=r
-    if amp<=0 or AMP<=0 or X<0 or X>B or x<0 or x>B or Y<0 or Y>A or y<0 or y>A: return -num.inf
+    (A,B) = dat.shape
+    (X,Y,AMP,x,y,amp) = r
+    if amp<=0 or AMP<=0 or X<0 or X>B or x<0 or x>B or Y<0 or Y>A or y<0 or y>A: return -np.inf
 
-    diff=psf.remove(X,Y,AMP,dat,useLinePSF=useLinePSF)
-    diff=psf.remove(x,y,amp,diff,useLinePSF=useLinePSF)
-    chi=-0.5*num.sum((diff**2/ue**2)[lims[0]:lims[1],lims[2]:lims[3]])
-    #chi=-num.sum(diff**2)**0.5
+    diff = psf.remove(X,Y,AMP,dat,useLinePSF=useLinePSF)
+    diff = psf.remove(x,y,amp,diff,useLinePSF=useLinePSF)
+    chi = -0.5*np.sum((diff**2/ue**2)[lims[0]:lims[1],lims[2]:lims[3]])
+    #chi=-np.sum(diff**2)**0.5
     if verbose:
         print '{:6d} {: 8.3f} {: 8.3f} {: 8.3f} {: 8.3f} {: 8.3f} {: 8.3f} {: 10.3f}'.format(psf.nForFitting,x,y,amp,X,Y,AMP,chi)
     return chi
@@ -91,17 +116,17 @@ class MCMCfitter:
         Use restore=fileName to import a dump fit file saved by saveState.
 
         """
-        self.psf=psf
-        self.imageData=num.copy(imageData)
-        self.fitted=False
+        self.psf = psf
+        self.imageData = np.copy(imageData)
+        self.fitted = False
 
         if restore:
             self._restoreState(restore)
 
-    def fitWithModelPSF(self,x_in,y_in,m_in=-1.,fitWidth=20,
-                        nWalkers=20,nBurn=10,nStep=20,
-                        bg=None, useErrorMap=False,
-                        useLinePSF=False, fitRateAngle = False,
+    def fitWithModelPSF(self,x_in,y_in,m_in = -1.,fitWidth = 20,
+                        nWalkers = 20,nBurn = 10,nStep = 20,
+                        bg = None, useErrorMap = False,
+                        useLinePSF = False, fitRateAngle = False,
                         rate_in = None, angle_in = None, exptime = None, pixScale = None,
                         verbose=False):
 
@@ -126,56 +151,59 @@ class MCMCfitter:
 
         print "Initializing sampler"
 
-        self.nForFitting=0
-        self.useLinePSF=useLinePSF
+        self.nForFitting = 0
+        self.useLinePSF = useLinePSF
 
         if fitWidth>self.psf.boxSize:
             raise NotImplementedError('Need to keep the fitWidth <= boxSize.')
 
-        (A,B)=self.imageData.shape
-        ai=max(0,int(y_in)-fitWidth)
-        bi=min(A,int(y_in)+fitWidth+1)
-        ci=max(0,int(x_in)-fitWidth)
-        di=min(B,int(x_in)+fitWidth+1)
-        dat=num.copy(self.imageData)
+        (A,B) = self.imageData.shape
+        ai = max(0,int(y_in)-fitWidth)
+        bi = min(A,int(y_in)+fitWidth+1)
+        ci = max(0,int(x_in)-fitWidth)
+        di = min(B,int(x_in)+fitWidth+1)
+        dat = np.copy(self.imageData)
 
 
 
-        if bg==None:
-            bgf=bgFinder.bgFinder(self.imageData)
-            bg=bgf.smartBackground()
-            dat-=bg
+        if bg == None:
+            bgf = bgFinder.bgFinder(self.imageData)
+            bg = bgf.smartBackground()
+            dat -= bg
             print 'Subtracting background {}'.format(bg)
 
         if not useErrorMap:
-            ue=dat*0.0+1.
+            ue = dat*0.0+1.
         else:
-            ue=(dat+bg)**0.5
+            ue = (dat+bg)**0.5
+
+        self.fitted = True
+
 
         if m_in==-1.:
             if useLinePSF:
-                m_in=self.psf.repFact*self.psf.repFact*num.sum(dat)/num.sum(self.psf.longPSF)
+                m_in = self.psf.repFact*self.psf.repFact*np.sum(dat)/np.sum(self.psf.longPSF)
             else:
-                m_in=self.psf.repFact*self.psf.repFact*num.sum(dat)/num.sum(self.psf.fullPSF)
+                m_in = self.psf.repFact*self.psf.repFact*np.sum(dat)/np.sum(self.psf.fullPSF)
 
         if not fitRateAngle:
 
-            nDim=2
-            r0=[]
+            nDim = 2
+            r0 = []
             for ii in range(nWalkers):
-                r0.append(num.array([x_in,y_in])+sci.randn(2)*num.array([0.1,0.1]))
-            r0=num.array(r0)
+                r0.append(np.array([x_in,y_in])+sci.randn(2)*np.array([0.1,0.1]))
+            r0 = np.array(r0)
 
             #fit first using input best guess amplitude
-            sampler=emcee.EnsembleSampler(nWalkers,nDim,lnprob,args=[dat,(ai,bi,ci,di),self.psf,ue,useLinePSF,verbose,(-1,-1,m_in)])
+            sampler = emcee.EnsembleSampler(nWalkers,nDim,lnprob,args=[dat,(ai,bi,ci,di),self.psf,ue,useLinePSF,verbose,(-1,-1,m_in)])
             print "Executing xy burn-in... this may take a while."
-            pos, prob, state=sampler.run_mcmc(r0, nBurn, 10)
+            pos, prob, state = sampler.run_mcmc(r0, nBurn)#, 10)
             sampler.reset()
             print "Executing xy production run... this will also take a while."
             pos, prob, state = sampler.run_mcmc(pos, nStep, rstate0=state)
-            self.samps=sampler.chain
-            self.probs=sampler.lnprobability
-            self.dat=num.copy(dat)
+            self.samps = sampler.chain
+            self.probs = sampler.lnprobability
+            self.dat = np.copy(dat)
 
             out = self.fitResults()
             (x,y,junk) = out[0]
@@ -185,8 +213,8 @@ class MCMCfitter:
             nDim = 1 # need to put two here rather than one because the fitresults code does a residual subtraction
             r0 = []
             for ii in range(nWalkers):
-                r0.append(num.array([m_in]) + sci.randn(1) * num.array([m_in*0.25]))
-            r0 = num.array(r0)
+                r0.append(np.array([m_in]) + sci.randn(1) * np.array([m_in*0.25]))
+            r0 = np.array(r0)
 
 
             #now fit the amplitude using the best-fit x,y from above
@@ -200,7 +228,7 @@ class MCMCfitter:
             pos, prob, state = sampler.run_mcmc(pos, max(nStep/2,10), rstate0=state)
             self.samps = sampler.chain
             self.probs = sampler.lnprobability
-            self.dat = num.copy(dat)
+            self.dat = np.copy(dat)
 
             out = self.fitResults()
             amp = out[0][0]
@@ -211,8 +239,8 @@ class MCMCfitter:
             nDim=3
             r0=[]
             for ii in range(nWalkers):
-                r0.append(num.array([x,y,amp])+sci.randn(3)*num.array([dx,dy,damp]))
-            r0=num.array(r0)
+                r0.append(np.array([x,y,amp])+sci.randn(3)*np.array([dx,dy,damp]))
+            r0=np.array(r0)
 
 
             sampler=emcee.EnsembleSampler(nWalkers,nDim,lnprob,args=[dat,(ai,bi,ci,di),self.psf,ue,useLinePSF,verbose])
@@ -224,29 +252,28 @@ class MCMCfitter:
 
         else:
 
-            nDim=5
-            r0=[]
+            nDim = 5
+            r0 = []
             for ii in range(nWalkers):
-                r0.append(num.array([x_in, y_in, m_in, rate_in, angle_in]) + sci.randn(nDim) * num.array([0.1, 0.1, m_in*0.1, rate_in*0.1, 4.0]))
-            r0=num.array(r0)
+                r0.append(np.array([x_in, y_in, m_in, rate_in, angle_in]) + sci.randn(nDim) * np.array([0.1, 0.1, m_in*0.1, rate_in*0.1, 4.0]))
+            r0 = np.array(r0)
 
             #fit first using input best guess amplitude
-            sampler=emcee.EnsembleSampler(nWalkers, nDim, lnprob_varRateAngle,
+            sampler = emcee.EnsembleSampler(nWalkers, nDim, lnprob_varRateAngle_LSSTHACK,
                                           args=[dat, (ai,bi,ci,di), self.psf, ue, useLinePSF, exptime, pixScale, verbose])
             #dat,lims,psf,ue,useLinePSF, exptime, pixScale, verbose=False):
             print "Executing burn-in... this may take a while."
-            pos, prob, state=sampler.run_mcmc(r0, nBurn, 10)
+            pos, prob, state = sampler.run_mcmc(r0, nBurn, 10)
             sampler.reset()
             print "Executing production run... this will also take a while."
             pos, prob, state = sampler.run_mcmc(pos, nStep, rstate0=state)
-            self.samps=sampler.chain
-            self.probs=sampler.lnprobability
-            self.dat=num.copy(dat)
+            self.samps = sampler.chain
+            self.probs = sampler.lnprobability
+            self.dat = np.copy(dat)
 
-        self.samps=sampler.chain
-        self.probs=sampler.lnprobability
-        self.dat=num.copy(dat)
-        self.fitted=True
+        self.samps = sampler.chain
+        self.probs = sampler.lnprobability
+        self.dat = np.copy(dat)
 
     def fitResults(self, confidenceRange=0.67, returnSamples=False):
         """
@@ -264,51 +291,51 @@ class MCMCfitter:
             print "You haven't actually run a fit yet!"
             return None
 
-        (Y,X,b)=self.samps.shape
+        (Y,X,b) = self.samps.shape
         goodSamps=[]
         for ii in range(Y):
             for jj in range(X):
-                g=[]
+                g = []
                 for kk in range(b):
                     g.append(self.samps[ii,jj][kk])
                 g.append(self.probs[ii,jj])
                 goodSamps.append(g)
-        goodSamps=num.array(goodSamps)
-        args=num.argsort(goodSamps[:,b])
-        goodSamps=goodSamps[args]
+        goodSamps = np.array(goodSamps)
+        args = np.argsort(goodSamps[:,b])
+        goodSamps = goodSamps[args]
 
-        bp=goodSamps[-1]
+        bp = goodSamps[-1]
         print 'Best point:',bp
         if b == 3:
-            self.residual=self.psf.remove(bp[0],bp[1],bp[2],self.dat,useLinePSF=self.useLinePSF)
-        elif b==6:
-            self.residual=self.psf.remove(bp[3],bp[4],bp[5],self.residual,useLinePSF=self.useLinePSF)
+            self.residual = self.psf.remove(bp[0],bp[1],bp[2],self.dat,useLinePSF=self.useLinePSF)
+        elif b == 6:
+            self.residual = self.psf.remove(bp[3],bp[4],bp[5],self.residual,useLinePSF=self.useLinePSF)
         elif b == 1:
             self.residual = None
-        self.fitFlux=num.sum(self.psf.model)*self.psf.fitFluxCorr
+        self.fitFlux = np.sum(self.psf.model)*self.psf.fitFluxCorr
 
         uncert=[]
         for ii in range(b):
-            args=num.argsort(goodSamps[:,ii])
-            x=goodSamps[args][:,ii]
-            a=len(x)*(1-confidenceRange)/2
-            b=1-a
+            args = np.argsort(goodSamps[:,ii])
+            x = goodSamps[args][:,ii]
+            a = len(x)*(1-confidenceRange)/2
+            b = 1-a
             uncert.append([x[int(a)],
                            x[int(b)]])
 
         if len(uncert)==6:
-            x=num.sort(goodSamps[:,5]/goodSamps[:,2])
+            x=np.sort(goodSamps[:,5]/goodSamps[:,2])
             a=len(x)*(1-confidenceRange)/2
             b=1-a
             uncert.append([x[int(a)],
                            x[int(b)]])
-            x = num.sort(goodSamps[:, 0] - goodSamps[:, 3])
+            x = np.sort(goodSamps[:, 0] - goodSamps[:, 3])
             uncert.append([x[int(a)],
                            x[int(b)]])
-            x = num.sort(goodSamps[:, 1] - goodSamps[:, 4])
+            x = np.sort(goodSamps[:, 1] - goodSamps[:, 4])
             uncert.append([x[int(a)],
                            x[int(b)]])
-            x = num.sort(((goodSamps[:, 1] - goodSamps[:, 4])**2+(goodSamps[:, 0] - goodSamps[:, 3])**2)**0.5)
+            x = np.sort(((goodSamps[:, 1] - goodSamps[:, 4])**2+(goodSamps[:, 0] - goodSamps[:, 3])**2)**0.5)
             uncert.append([x[int(a)],
                            x[int(b)]])
         if not returnSamples: return (bp, uncert)
@@ -340,54 +367,54 @@ class MCMCfitter:
         """
 
 
-        self.useLinePSF=useLinePSF
+        self.useLinePSF = useLinePSF
 
-        (A,B)=self.imageData.shape
-        ai=max(0,int((y_in+Y_in)/2)-fitWidth)
-        bi=min(A,int((y_in+Y_in)/2)+fitWidth+1)
-        ci=max(0,int((x_in+X_in)/2)-fitWidth)
-        di=min(B,int((x_in+X_in)/2)+fitWidth+1)
-        dat=num.copy(self.imageData)
+        (A,B) = self.imageData.shape
+        ai = max(0,int((y_in+Y_in)/2)-fitWidth)
+        bi = min(A,int((y_in+Y_in)/2)+fitWidth+1)
+        ci = max(0,int((x_in+X_in)/2)-fitWidth)
+        di = min(B,int((x_in+X_in)/2)+fitWidth+1)
+        dat = np.copy(self.imageData)
 
 
         if bg==None:
-            bgf=bgFinder.bgFinder(self.imageData)
-            bg=bgf.smartBackground()
-            dat-=bg
+            bgf = bgFinder.bgFinder(self.imageData)
+            bg = bgf.smartBackground()
+            dat -= bg
 
 
         if not useErrorMap:
-            ue=dat*0.0+1.
+            ue = dat*0.0+1.
         else:
-            ue=(dat+bg)**0.5
+            ue = (dat+bg)**0.5
 
 
 
-        if m_in==-1.:
+        if m_in == -1.:
             if useLinePSF:
-                m_in=num.sum(dat)/num.sum(self.psf.longPSF)
+                m_in = np.sum(dat)/np.sum(self.psf.longPSF)
             else:
-                m_in=num.sum(dat)/num.sum(self.psf.fullPSF)
+                m_in = np.sum(dat)/np.sum(self.psf.fullPSF)
 
         nDim=6
         r0=[]
         for ii in range(nWalkers):
-            r0.append(num.array([x_in,y_in,m_in,X_in,Y_in,m_in*bRat_in])+sci.randn(6)*num.array([1.,1.,
+            r0.append(np.array([x_in,y_in,m_in,X_in,Y_in,m_in*bRat_in])+sci.randn(6)*np.array([1.,1.,
                                                                                                           m_in*0.4,
                                                                                                     1.,1.,
                                                                                                m_in*0.4*bRat_in]))
-        r0=num.array(r0)
+        r0=np.array(r0)
 
-        sampler=emcee.EnsembleSampler(nWalkers,nDim,lnprobDouble,args=[dat,(ai,bi,ci,di),self.psf,ue,self.useLinePSF,verbose])
-        pos, prob, state=sampler.run_mcmc(r0,nBurn)
+        sampler = emcee.EnsembleSampler(nWalkers,nDim,lnprobDouble,args=[dat,(ai,bi,ci,di),self.psf,ue,self.useLinePSF,verbose])
+        pos, prob, state = sampler.run_mcmc(r0,nBurn)
         sampler.reset()
         pos, prob, state = sampler.run_mcmc(pos, nStep, rstate0=state)
-        self.samps=sampler.chain
-        self.probs=sampler.lnprobability
-        self.dat=num.copy(dat)
-        self.fitted=True
+        self.samps = sampler.chain
+        self.probs = sampler.lnprobability
+        self.dat = np.copy(dat)
+        self.fitted = True
 
-    def saveState(self,fn='MCState.pickle'):
+    def saveState(self, fn = 'MCState.pickle'):
         """
         Save the fitted state to the provided filename.
         """
