230e726fd2f5d823b261201848e3bccd691dcc44
fraserw <westhefras@gmail.com>
2019-10-30 16:15:08 -0700

    --version tick --readded tzscale because its useful to have around

--- trippy/__init__.py
+++ trippy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-__version__='1.1.1'
+__version__='1.2'
 __author__='Wesley Fraser (github: fraserw, westhefras@gmail.com)'
-__all__=['psf','pill','bgFinder','utils','psfStarChooser','MCMCfit','scamp']
+__all__=['psf','pill','bgFinder','utils','psfStarChooser','MCMCfit','scamp','tzscale']
 
 from . import psf,pill,bgFinder
 #from . import *
